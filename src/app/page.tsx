bd980f59e4665f3b607ff277542e50f691524c85
kanadomekei <t0520keita@icloud.com>
2024-08-23 19:54:18 +0900

    ホームページのレイアウトをもとに戻す

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -97,7 +97,7 @@ export default function Home() {
           target='_blank'
           rel='noopener noreferrer'
         >
-          {/* <h2 className='mb-3 text-2xl font-semibold'>
+          <h2 className='mb-3 text-2xl font-semibold'>
             Deploy{' '}
             <span className='inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none'>
               -&gt;
@@ -105,7 +105,7 @@ export default function Home() {
           </h2>
           <p className='m-0 max-w-[30ch] text-balance text-sm opacity-50'>
             Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p> */}
+          </p>
         </a>
       </div>
     </main>
